be911dc1c2903a717a9c1efe965c3cff2b6ef6d9
Rafaël Freyssinet <48367042+rafaelfreyssinet@users.noreply.github.com>
2024-09-08 21:37:46 +0200

    Initial commit

---
+++ src/main/java/com/lunaris/lunarismod/LunarisModClient.java
@@ -0,0 +1,10 @@
+package com.lunaris.lunarismod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LunarisModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/lunaris/lunarismod/LunarisModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lunaris.lunarismod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LunarisModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
